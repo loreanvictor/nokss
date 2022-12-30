434a26678d73cc004bbc58409900e8dd094eb1e9
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2022-12-30 16:12:06 +0100

    add header links

--- docs/assets/index.js
+++ docs/assets/index.js
@@ -2,6 +2,15 @@ import select from 'https://esm.sh/select'
 import { html, ref } from 'https://esm.sh/rehtm'
 
 
+const copy = text => {
+  const el = ref()
+  document.body.append(html`<textarea ref=${el} style="{opacity: 0; width: 0; height: 0}">${text}</textarea>`)
+  select(el.current)
+  document.execCommand('copy')
+  el.current.remove()
+}
+
+
 const addCopyButtons = () => {
   document.querySelectorAll('pre').forEach(pre => {
     const btn = ref()
@@ -22,4 +31,21 @@ const addCopyButtons = () => {
 }
 
 
+const addHeaderLinks = () => {
+  document.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach(h => {
+    const id = h.id
+    const btn = ref()
+
+    const copyLink = () => {
+      copy(location.protocol + '//' + location.host + location.pathname + '#' + id)
+      btn.current.textContent = 'done'
+      setTimeout(() => btn.current.textContent = 'link', 2000)
+    }
+
+    h.append(html`<menu role=toolbar><button ref=${btn} onclick=${copyLink} class=material-icons>link</button></menu>`)
+  })
+}
+
+
 addCopyButtons()
+addHeaderLinks()
