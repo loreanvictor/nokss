b82c59ac7d615663a61c4eb433abfa0e0f1e67f4
loreanvictor <ghanizadeh.eugene@gmail.com>
2023-01-01 11:47:41 +0000

    Deploying to gh-pages from @ loreanvictor/nokss@b2eb2c54f7a4223451bf19109c4814f868cbfc93 🚀

--- assets/index.js
+++ assets/index.js
@@ -32,7 +32,7 @@ const addCopyButtons = () => {
 
 
 const addHeaderLinks = () => {
-  document.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach(h => {
+  document.querySelectorAll('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]').forEach(h => {
     const id = h.id
     const btn = ref()
 
