ac4cb2513d2ba69f56d01290d70b84de4b0c243d
loreanvictor <ghanizadeh.eugene@gmail.com>
2023-01-04 19:05:54 +0000

    Deploying to gh-pages from @ loreanvictor/nokss@afb8253c3e7f65eb46c88d3489c70deb4a2440e3 🚀

---
+++ assets/footer.js
@@ -0,0 +1,17 @@
+const control = document.querySelector('footer button[role="switch"]')
+const sidebar = document.querySelector('body>nav:first-child')
+
+control.addEventListener('click', () => {
+  sidebar.setAttribute('aria-hidden',control.getAttribute('aria-checked'))
+})
+
+if (window.matchMedia('(max-width: 1024px)').matches) {
+  control.setAttribute('aria-checked', 'true')
+
+  sidebar.querySelectorAll('a').forEach(anchor => {
+    anchor.addEventListener('click', () => {
+      control.setAttribute('aria-checked', 'true')
+      sidebar.setAttribute('aria-hidden', 'true')
+    })
+  })
+}
